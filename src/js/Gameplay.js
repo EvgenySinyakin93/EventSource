0ef6080af32ad6495d96203b069d193cb957a45a
EvgenySinyakin93 <evgeny.siniakin83@gmail.com>
2024-06-07 11:19:13 +0300

    first commit

--- src/js/Gameplay.js
+++ src/js/Gameplay.js
@@ -1,9 +1,36 @@
 export default class GamePlay {
-  constructor(field) {
-    this._field = field;
+  constructor(fields) {
+    this.fields = fields;
+    this.size = 4;
+    this.board = null;
+    this.position = 0;
   }
 
-  goGoblin() {
-    const fields = document.querySelectorAll('.box');
+  init() {
+    this.board = this.fields.gameField()
+    this.start();
+  }
+
+  setPosition () {
+    const position = Math.floor(Math.random() * this.size ** 2);
+    this.delPosition(this.position);
+    if (position === this.position) {
+      this.setPosition();
+      return;
+    }
+    else {
+      this.board.children[position].classList.add('goblin');
+      this.position = position;      
+    }
+  }
+
+  delPosition (position) {
+    this.board.children[position].classList.remove('goblin');
+  }
+
+  start() {
+    setInterval(() => {
+      this.setPosition();
+    }, 1000);
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
-import GamePlay from './GamePlay';
+import Field from './Field';
+import GamePlay from './Gameplay';
 
-
+const fields = new Field();
 const gameplay = new GamePlay(fields);
+gameplay.init();
