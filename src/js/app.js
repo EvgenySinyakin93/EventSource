bc8824c1e716981e1fbce2cfa0321b0a8d87c75e
EvgenySinyakin93 <evgeny.siniakin83@gmail.com>
2024-07-03 19:26:00 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,19 @@
-import Character from "./Init";
+import validate from "./validator";
+import paySystem from "./paySystem";
 
-const Game = new Character(document.querySelector(".grid-container"));
-Game.init();
+document.querySelector("#card-submit").addEventListener("click", (event) => {
+  event.preventDefault();
+  const num = document.querySelector("#numberCard-input").value;
+  if (validate(num)) {
+    paySystem(num);
+  } else {
+    alert("Invalid card number");
+  }
+});
+document.querySelector("#input-reset").addEventListener((event) => {
+  event.preventDefault();
+  document.form[0].reset();
+  document.querySelectorAll(".card-item").forEach((el) => {
+    el.classList.remove("active");
+  });
+});
