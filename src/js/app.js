72a8ea5fb66a4dcf3017e7f7a14d1096be42ff26
EvgenySinyakin93 <evgeny.siniakin83@gmail.com>
2024-08-02 21:22:27 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,24 @@
-import WidgetCollapsible from "./widgetCollapsible";
-import WidgetChat from "./widgetChat";
-import WidgetLikes from "./widgetLikes";
-
-const widgetCollapsible = new WidgetCollapsible();
-widgetCollapsible.init();
-
-const widgetChat = new WidgetChat();
-widgetChat.init();
-
-const widgetLikes = new WidgetLikes();
-widgetLikes.init();
+export default function checkCoordinatesValidity(inputValue) {
+    let newValue;
+    const validObj = {};
+  
+    if (inputValue.startsWith("[") && inputValue.endsWith("]")) {
+      newValue = inputValue.slice(1, inputValue.length - 1).split(",");
+    } else {
+      newValue = inputValue.split(",");
+    }
+  
+    if (newValue.length !== 2) return validObj;
+    const lat = parseFloat(newValue[0].trim());
+    const lng = parseFloat(newValue[1].trim());
+  
+    if (!Number.isNaN(lat) && Math.abs(lat) <= 90) {
+      validObj.lat = lat;
+    }
+  
+    if (!Number.isNaN(lng) && Math.abs(lng) <= 180) {
+      validObj.lng = lng;
+    }
+  
+    return validObj;
+  }
